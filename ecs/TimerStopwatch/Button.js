28fc5aba448a078dd777cb6af856d7b2b677de4f
EnderMan722 <9683176@graniteschools.org>
2026-03-30 10:16:03 -0600

    Add Button class with display and interaction methods

--- ecs/TimerStopwatch/Button.js
+++ ecs/TimerStopwatch/Button.js
@@ -1,1 +1,47 @@
+class Button {
+  constructor(label, x, y, w, h) {
+    this.label = label;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.buttonColor = 220;
+  }
 
+  display() {
+    if (this.isMouseOver()) {
+      this.buttonColor = 190;
+    } else {
+      this.buttonColor = 220;
+    }
+
+    fill(this.buttonColor);
+    stroke(0);
+
+    ellipseMode(CENTER);
+    ellipse(this.x, this.y, this.w, this.h);
+
+    fill(0);
+    textAlign(CENTER, CENTER);
+    textSize(16);
+    text(this.label, this.x, this.y - 2);
+  }
+
+  clicked(mx, my) {
+    return (
+      mx > this.x - this.w / 2 &&
+      mx < this.x + this.w / 2 &&
+      my > this.y - this.h / 2 &&
+      my < this.y + this.h / 2
+    );
+  }
+
+  isMouseOver() {
+    return (
+      mouseX > this.x - this.w / 2 &&
+      mouseX < this.x + this.w / 2 &&
+      mouseY > this.y - this.h / 2 &&
+      mouseY < this.y + this.h / 2
+    );
+  }
+}
